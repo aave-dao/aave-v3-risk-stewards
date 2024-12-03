a60518c12275d2826ed45b5a15097ba1b3470c4f
MartinGbz <martin.grabarz@outlook.fr>
2024-12-03 16:15:59 +0100

    🐛 fix risk steward import issue (#21)
    * 🐛 fix issue
    
    * 🐛 avoid using a temp var

--- generator/templates/proposal.template.ts
+++ generator/templates/proposal.template.ts
@@ -6,10 +6,11 @@ import {prefixWithPragma} from '../utils/constants';
 export const proposalTemplate = (
   options: Options,
   poolConfig: PoolConfig,
-  pool: PoolIdentifier,
+  pool: PoolIdentifier
 ) => {
   const {title, author, discussion} = options;
   const chain = getPoolChain(pool);
+
   const folderName = generateFolderName(options);
   const contractName = generateContractName(options, pool);
 
@@ -23,10 +24,11 @@ export const proposalTemplate = (
   * @title ${title || 'TODO'}
   * @author ${author || 'TODO'}
   * - discussion: ${discussion || 'TODO'}
-  * - deploy-command: make run-script contract=src/contracts/updates/${folderName}/${contractName}.sol:${contractName} network=${getChainAlias(chain)} broadcast=false generate_diff=true
+  * - deploy-command: make run-script contract=src/contracts/updates/${folderName}/${contractName}.sol:${contractName} network=${getChainAlias(
+    chain
+  )} broadcast=false generate_diff=true
   */
- contract ${contractName} is ${`RiskStewards${chain}`
- } {
+ contract ${contractName} is ${`RiskStewards${chain === 'Base' ? 'BaseChain' : chain}`} {
   function name() public pure override returns (string memory) {
     return '${contractName}';
   }
