785087a297909421aca42f76d954722f8125dfeb
MartinGbz <martin.grabarz@outlook.fr>
2024-12-03 11:13:43 +0100

    🐛 fix issue

--- generator/templates/proposal.template.ts
+++ generator/templates/proposal.template.ts
@@ -6,10 +6,14 @@ import {prefixWithPragma} from '../utils/constants';
 export const proposalTemplate = (
   options: Options,
   poolConfig: PoolConfig,
-  pool: PoolIdentifier,
+  pool: PoolIdentifier
 ) => {
   const {title, author, discussion} = options;
   const chain = getPoolChain(pool);
+
+  // Edge case: use BaseChain instead of Base for RiskStewards network
+  const riskStewardChain = chain === 'Base' ? 'BaseChain' : chain;
+
   const folderName = generateFolderName(options);
   const contractName = generateContractName(options, pool);
 
@@ -23,10 +27,11 @@ export const proposalTemplate = (
   * @title ${title || 'TODO'}
   * @author ${author || 'TODO'}
   * - discussion: ${discussion || 'TODO'}
-  * - deploy-command: make run-script contract=src/contracts/updates/${folderName}/${contractName}.sol:${contractName} network=${getChainAlias(chain)} broadcast=false generate_diff=true
+  * - deploy-command: make run-script contract=src/contracts/updates/${folderName}/${contractName}.sol:${contractName} network=${getChainAlias(
+    chain
+  )} broadcast=false generate_diff=true
   */
- contract ${contractName} is ${`RiskStewards${chain}`
- } {
+ contract ${contractName} is ${`RiskStewards${riskStewardChain}`} {
   function name() public pure override returns (string memory) {
     return '${contractName}';
   }
