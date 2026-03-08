6de0b13a2ef75adae834ba4e70988528bedf109b
MartinGbz <martin.grabarz@outlook.fr>
2026-03-08 02:05:43 -0500

    ⬆️ use @aave-dao/aave-address-book (#64)
    * ⬆️ use @aave-dao/aave-address-book instead of @bgd-labs/aave-address-book
    
    * ⬆️ update lib in code
    
    * 🔧 Update .gitmodules
    
    * ⬆️ update aave-helpers

--- generator/common.ts
+++ generator/common.ts
@@ -1,4 +1,4 @@
-import * as addressBook from '@bgd-labs/aave-address-book';
+import * as addressBook from '@aave-dao/aave-address-book';
 import {Options, PoolIdentifier, PoolIdentifierV3} from './types';
 import {
   arbitrum,
